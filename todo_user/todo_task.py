6600166d3b1f4658e964dccf3886c0707eeb7ad4
israel <irebollo@binovo.es>
2017-03-06 09:29:24 +0100

    modificado:    ../.idea/workspace.xml 	modificado:    ../todo_app/.idea/workspace.xml 	modificado:    ../todo_app/todo_view.xml 	modificado:    security/todo_access_rules.xml 	modificado:    todo_task.py 	modificado:    todo_task.pyc 	modificado:    todo_view.xml

--- todo_user/todo_task.py
+++ todo_user/todo_task.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api
 class TodoTask(models.Model):
-    _inherit = 'todo.task'
+    _name = 'todo.task'
+    _inherit = ['todo.task','mail.thread']
     name = fields.Char(help="¿Que necesitas?")
     user_id = fields.Many2one('res.users', 'Responsible')
     date_deadline = fields.Date('Deadline')
